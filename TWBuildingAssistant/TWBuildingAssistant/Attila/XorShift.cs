a9073efc3b90c6e9d6c04669aabb93163e61c016
Paweł Matusek <pmatusek1@wp.pl>
2018-03-02 19:18:42 +0100

    Actual initial commit.

---
+++ TWBuildingAssistant/TWBuildingAssistant/Attila/XorShift.cs
@@ -0,0 +1,30 @@
+﻿namespace TWAssistant
+{
+	namespace Attila
+	{
+		public class XorShift
+		{
+			uint x;
+			uint y;
+			uint z;
+			public XorShift(uint seed)
+			{
+				x = 0x075bcd15 + seed;
+				y = 0x159a55e5;
+				z = 0x1f123bb5;
+			}
+			public uint Next(uint lowerLimit, uint upperLimit)
+			{
+				uint t;
+				x ^= x << 16;
+				x ^= x >> 5;
+				x ^= x << 1;
+				t = x;
+				x = y;
+				y = z;
+				z = t ^ x ^ y;
+				return (z % (upperLimit - lowerLimit)) + lowerLimit;
+			}
+		}
+	}
+}

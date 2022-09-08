d756e97d2c936c080fa8952a70a699c25b343d30
pawlakov <pmatusek@protonmail.com>
2022-09-08 22:23:16 +0200

    static app from template

---
+++ TWBuildingAssistant/TWBuildingAssistant.StaticApp.Api/WeatherForecastFunction.cs
@@ -0,0 +1,55 @@
+﻿namespace TWBuildingAssistant.StaticApp.Api;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TWBuildingAssistant.StaticApp.Shared;
+
+public static class WeatherForecastFunction
+{
+    private static string GetSummary(int temp)
+    {
+        var summary = "Mild";
+
+        if (temp >= 32)
+        {
+            summary = "Hot";
+        }
+        else if (temp <= 16 && temp > 0)
+        {
+            summary = "Cold";
+        }
+        else if (temp <= 0)
+        {
+            summary = "Freezing!";
+        }
+
+        return summary;
+    }
+
+    [FunctionName("WeatherForecast")]
+    public static IActionResult Run(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+        ILogger log)
+    {
+        var randomNumber = new Random();
+        var temp = 0;
+
+        var result = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateTime.Now.AddDays(index),
+            TemperatureC = temp = randomNumber.Next(-20, 55),
+            Summary = GetSummary(temp)
+        }).ToArray();
+
+        return new OkObjectResult(result);
+    }
+}
